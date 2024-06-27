283555d93779d30296f25e35ac29f48cb3f531e2
Michael de Souza Marcos <michael.smarcos@hotmail.com>
2024-06-27 10:13:21 -0300

    feat: improve output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
-output "VMs_And_IPs" {
-  value = ["VM: ${element(virtualbox_vm.ubuntu[*].name, 1)} | IP: ${element(virtualbox_vm.ubuntu[*].network_adapter[0].ipv4_address, 1)}"]
+output "VMs_And_IPs_VM_Ubuntu" {
+  value = {
+    for vm_id, vm in virtualbox_vm.ubuntu :
+    vm_id => {
+      VM = vm.name
+      IP = vm.network_adapter[0].ipv4_address
+    }
+  }
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,5 @@
-number_of_vms          = 1
+number_of_vms          = 3
 bridged_host_interface = "wlo1"
 vagrant_ubuntu_box     = "https://app.vagrantup.com/ubuntu/boxes/focal64/versions/20240612.0.1/providers/virtualbox.box"
-number_of_cpus         = 2
-ram_size               = "1024 mb"
+number_of_cpus         = 1
+ram_size               = "512 mb"
